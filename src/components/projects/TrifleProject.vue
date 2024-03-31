41766aebf6677ac374547fd25016a527b90fa6cf
lynn2910 <cedric.colin35@gmail.com>
2024-03-31 18:28:49 +0200

    Projet Trifle

--- src/components/projects/TrifleProject.vue
+++ src/components/projects/TrifleProject.vue
@@ -1,7 +1,16 @@
 <script setup lang="ts">
+import ProjectSection, { type ProjectViewHeaders } from '@/components/project/ProjectSection.vue'
+import SaeToast from '@/components/projects/comp/SaeToast.vue'
 
+let headers: ProjectViewHeaders = [];
 </script>
 
 <template>
-  <p>Trifle - Kamisado</p>
+  <ProjectSection :headers="headers" title="Trifle - Kamisado">
+    <SaeToast></SaeToast>
+    <br><br>
+    <div class="toast success">
+      <p><strong>Cette SAE n'est pas encore commencée. Cette page sera mise à jour quand la SAE sera bien avancée.</strong></p>
+    </div>
+  </ProjectSection>
 </template>
