d6b2fe37aa801d9fa660df6cbc2d029ebd1d13aa
lynn2910 <cedric.colin35@gmail.com>
2025-05-22 23:29:03 +0200

    feat(navbar): Better responsive pattern for left and right elements

--- src/lib/components/navigation/Navbar.svelte
+++ src/lib/components/navigation/Navbar.svelte
@@ -2,7 +2,6 @@
 	import type {Snippet} from "svelte";
 
 	const {
-		top_padding = "sm:top-6",
 		left,
 		main,
 		right,
@@ -19,49 +18,55 @@
 
 	function toggleNav() {
 		opened = !opened;
+
+		if (opened) document.body.style.overflow = "hidden";
 	}
 </script>
 
-<button
-        class="fixed z-50 max-sm:top-3 max-sm:left-3 sm:hidden p-2 h-10 w-10 rounded-full bg-brown-coffee text-old-lace"
-        onclick={toggleNav}
-        aria-label={opened ? "Fermer la navigation" : "Ouvrir la navigation"}
->
-    {#if opened}
-        ✖
-    {:else}
-        ☰
-    {/if}
-</button>
+<nav class="overflow-hidden">
+    <button
+            class="fixed z-50 max-sm:top-3 max-sm:left-3 sm:hidden p-2 h-10 w-10 rounded-full bg-brown-coffee text-old-lace"
+            onclick={toggleNav}
+            aria-label={opened ? "Fermer la navigation" : "Ouvrir la navigation"}
+    >
+        {#if opened}
+            ✖
+        {:else}
+            ☰
+        {/if}
+    </button>
 
-<nav
-        class="fixed w-full h-full z-40 bg-brown-coffee text-old-lace
+    <div
+            class="fixed w-full h-full z-40 bg-brown-coffee text-old-lace overflow-hidden
            sm:top-2 sm:w-[calc(100vw-1rem)] sm:mx-2 sm:h-14 sm:rounded-4xl sm:py-3 sm:px-6
            max-sm:transition-transform max-sm:duration-300 max-sm:ease-in-out
            "
-        class:max-sm:-translate-x-full={!opened}
-        class:max-sm:translate-x-0={opened}
-        class:max-sm:top-0={opened}
-        class:max-sm:left-0={opened}
-        hidden={hidden}
->
-    {@render main()}
+            class:max-sm:-translate-x-full={!opened}
+            class:max-sm:translate-x-0={opened}
+            class:max-sm:top-0={opened}
+            class:max-sm:left-0={opened}
+            hidden={hidden}
+    >
+        {@render main()}
+    </div>
+
+    {#if left}
+        <div
+                class={"overflow-hidden fixed z-40 left-5 sm:left-6 max-sm:top-16 sm:top-2 sm:h-14 flex flex-row items-center" + (!opened && ' max-sm:hidden')}
+                hidden={hidden}
+        >
+            {@render left()}
+        </div>
+    {/if}
+
+
+    {#if right}
+        <div
+                class={"overflow-hidden fixed z-40 max-sm:bottom-6 max-sm:left-1/2 max-sm:-translate-x-1/2 max-sm:h-fit sm:right-6 sm:top-2 sm:h-14 flex flex-row items-center " + (!opened && 'max-sm:hidden')}
+                hidden={hidden}
+        >
+            {@render right()}
+        </div>
+    {/if}
+
 </nav>
-
-{#if left}
-    <div
-            class={"fixed z-40 left-5 sm:left-6 max-sm:top-16 sm:top-6 " + (!opened && 'max-sm:hidden')}
-            hidden={hidden}
-    >
-        {@render left()}
-    </div>
-{/if}
-
-{#if right}
-    <div
-            class={"fixed z-40 max-sm:bottom-6 max-sm:left-1/2 max-sm:-translate-x-1/2 max-sm:h-fit sm:right-6 " + top_padding + " " + (!opened && 'max-sm:hidden')}
-            hidden={hidden}
-    >
-        {@render right()}
-    </div>
-{/if}
